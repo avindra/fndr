9e1bbc452154d3a0a5f94c1f290f68a5470d6186
Avindra Goolcharan <aavindraa@gmail.com>
2021-07-03 17:29:42 -0400

    Implement Knowledge ID lookup

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+# Changelog
+
+## 0.3.0 - 07/03/2021
+
+* add `id` feature to lookup Knowledge Graph ids
+
+## 0.2.1
+
+* use URLSearchParams to serialize URL
+
+## 0.2.0
+
+* --big to look for big images
+* initial DuckDuckGo support
+* use DDG for "lucky" feat
+
+## 0.1.0 - 06/25/2021
+
+* Initial import based on powersearch 0.2 rework
+* change to unsafe by default
--- src/id.js
+++ src/id.js
@@ -5,12 +5,33 @@
  *  - Google Knowledge Graph id (freebase + regular)
  */
 
+const kgmbase = `https://www.google.com/search?kgmid=`;
+
+const headers = new Headers({
+	"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0"
+});
+
+const bannedIds = [
+	"/m/0jg24" // basic "Image" concept
+];
+
 export async function getId(input) {
-	const response = await fetch(`http://google.com/search?q=${input}`);
+	const response = await fetch(`http://google.com/search?q=${input}`, {
+		headers
+	});
 	const text = await response.text();
-	const results = text.match(/\\x22(\/[gm]\/.+)\\x22/);
+
+	let results = text.match(/22\/[gm]\/[^\\]+/g);
+
 	if (results) {
-		return results[1];
+		results = results.map(s => s.substring(2))
+			.filter(id => !bannedIds.includes(id));
+
+		if (results.length == 1) {
+			return kgmbase + results[0];
+		} else {
+			console.warn("Non-singular result", results);
+		}
 	}
 
 	return false;
