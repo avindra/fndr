040428aa72ae740739a0c111e8ad8141a52201d5
Avindra Goolcharan <aavindraa@gmail.com>
2021-07-06 03:41:15 -0400

    Cleanup engine handling

---
+++ src/search.ts
@@ -0,0 +1,69 @@
+
+import { exec } from "https://deno.land/x/exec/mod.ts";
+
+const engines = [
+	"https://images.google.com/images",
+	"https://news.google.com/news",
+	"https://maps.google.com/maps",
+	"https://images.bing.com/",
+	"https://duckduckgo.com/",
+];
+
+/**
+ * @permission exec
+ */
+const goto = async function(base: string = "http://www.", path: string, unsafe: boolean) {
+	const executor = unsafe ? `firefox -private-window` : "xdg-open";
+	const line = `${executor} "${base + path}"`;
+	console.log("running", line);
+	await exec(line);
+};
+
+/***
+ * 
+ * id is a number corresponding to a particular Search engine.
+ * should be an enum
+ * 
+ * returns the string pair [url', query]
+ * 
+ * where url' is the partial URL leading up to the first ?
+ * 
+ * @note does not perform a search
+ */
+export const searchFor = function(
+	id: number,
+	query: string,
+	isBig: boolean,
+	unsafe: boolean
+) {
+	const e = engines[id];
+	const isBing = 3 == id;
+	const isDDG = 4 == id;
+	isBing && (e = "https://images.bing.com/");
+	isDDG && (e = "https://duckduckgo.com/");
+	let params = new URLSearchParams();
+	if (unsafe) {
+		if (isDDG) {
+			params.set("kp", "-2");
+		} else if(isBing) {
+			params.set("safesearch", "off");
+		} else { // assume google
+			params.set("safe", "images");
+		}
+	}
+	if (isDDG) {
+		params.set("iax", "images");
+		params.set("ia", "images");
+	}
+	if (isBig) {
+		if (isBing) {
+			params.set("qft", "+filterui:imagesize-custom_1920_1080");
+		} else if(isDDG) {
+			params.set("iaf", "size:Large");
+		} else { // assume google
+			params.set("tbs", "isz:lt,islt:2mp");
+		}
+	}
+	params.set("q", query);
+	goto(e, "?" + params.toString(), unsafe);
+};
