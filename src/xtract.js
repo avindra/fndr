c4189d286f6fb90814be41a2ef7d69e919ad7552
Avindra Goolcharan <aavindraa@gmail.com>
2021-07-20 02:37:07 -0400

    tripadvisor extract

--- src/xtract.js
+++ src/xtract.js
@@ -16,20 +16,32 @@ const ids = {
 	"vimeo.com": /vimeo.com\/([^\/]+)/,
 	"pinterest.de": /pinterest.de\/(^\/]+)/,
 	"tripadvisor.at": /tripadvisor.at\/(^\/]+)/,
+	"tripadvisor.com": /(g\d+-d\d+)/,
 }
 
 export const getSocial = (body) => {
-	return [...new Set(getLinks(body))].filter(link => {
+	return [...new Set(getLinks(body))].map(
+		link => {
+			try {
+				const u = new URL(link);
+				// drop subdomain specifications
+				u.host = u.host.replace(/(.+)\.(.+)\.com/, '$2.com');
+				return u.toString();
+			} catch(e) {
+				return link;
+			}
+	}).filter(link => {
 		try {
 			const u = new URL(link);
-			const base = u.host.replace('www.', '');
+			const base = u.host;
+
 			return base in ids;
 		} catch(e) {} // URL throws on malformed URLS, just ignore for now
 
 		return false;
 	}).map(link => {
 		const u = new URL(link);
-		const base = u.host.replace('www.', '');
+		const base = u.host;
 		const reg = ids[base];
 		if (reg.test(link)) {
 			return [base, link.match(reg)[1]];
