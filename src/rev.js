0c66b88c2121b0cec6a27bcc682a4d43e841493f
Avindra Goolcharan <aavindraa@gmail.com>
2021-07-31 16:23:06 -0400

    Integrate reverse image search

---
+++ src/rev.js
@@ -0,0 +1,36 @@
+import {goto} from './search.ts';
+/**
+ * Perform reverse image search
+ *
+ * @todo do this using `fetch` instead of `curl`
+ * @see https://stackoverflow.com/a/62088422/270302
+ */
+export const rev = async () => {
+	const p = Deno.run({
+		cmd: [
+			"curl",
+			"-F",
+			"encoded_image=@/dev/stdin",
+			"https://www.google.com/searchbyimage/upload"
+		],
+		stdout: "piped",
+		stdin: "piped"
+	});
+
+	const decoder = new TextDecoder();
+
+	await Deno.copy(Deno.stdin, p.stdin);
+
+	await p.stdin.close();
+	const output = await p.output()
+	p.close();
+
+	const txt = decoder.decode(output);
+
+	const g = txt.match(/HREF="([^"]+)/);
+	if(g) {
+		goto(g[1])
+	} else {
+		console.log(txt);
+	}
+}
