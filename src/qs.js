a215e7cf46727c268cfe428c1ce8fc124afe299d
Avindra Goolcharan <aavindraa@gmail.com>
2022-10-13 16:20:15 -0400

    clean google links

--- src/qs.js
+++ src/qs.js
@@ -4,21 +4,37 @@
  * from QuickStatements (useful for
  * asynchronous use of Wikidata).
 */
-export function getClean(input) {
-	var raw = decodeURIComponent(input);
+
+function cleanQuickStatement(input) {
+	const raw = decodeURIComponent(input);
 
 	// just get v1= value
-	var idx = raw.indexOf('#') + 5;
+	const idx = raw.indexOf('#') + 5;
 
 
-	var base = raw.substring(0, idx);
+	const base = raw.substring(0, idx);
 
-	var query = raw.substring(idx);
+	const query = raw.substring(idx);
+
 
 	var cleanQuery = query.replace(/S4656\|[^\|]+/g, '');
 	cleanQuery = cleanQuery.replace(/P18\|[^\|]+/g, '');
+	cleanQuery = cleanQuery.replace(/P143\|[^\|]+/g, '');
 
 	return base + encodeURIComponent(cleanQuery);
+}
+
+function cleanGoogleLink(input) {
+	const U = new URL(input);
+	const params = new URLSearchParams(U.search);
+	return params.get("url");
+}
+
+export function getClean(input) {
+	if (input.includes("google.com")) {
+		return cleanGoogleLink(input);
+	}
+	return cleanQuickStatement(input);
 }
 
 
