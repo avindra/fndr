3ca11c42a293c0c31efb08850c5647f03a067436
Avindra Goolcharan <aavindraa@gmail.com>
2023-05-03 16:15:53 -0400

    rome format

--- src/qs.js
+++ src/qs.js
@@ -1,25 +1,22 @@
-
 /**
  * Trim image and URL attributes
  * from QuickStatements (useful for
  * asynchronous use of Wikidata).
-*/
+ */
 
 function cleanQuickStatement(input) {
 	const raw = decodeURIComponent(input);
 
 	// just get v1= value
-	const idx = raw.indexOf('#') + 5;
-
+	const idx = raw.indexOf("#") + 5;
 
 	const base = raw.substring(0, idx);
 
 	const query = raw.substring(idx);
 
-
-	var cleanQuery = query.replace(/S4656\|[^\|]+/g, '');
-	cleanQuery = cleanQuery.replace(/P18\|[^\|]+/g, '');
-	cleanQuery = cleanQuery.replace(/P143\|[^\|]+/g, '');
+	var cleanQuery = query.replace(/S4656\|[^\|]+/g, "");
+	cleanQuery = cleanQuery.replace(/P18\|[^\|]+/g, "");
+	cleanQuery = cleanQuery.replace(/P143\|[^\|]+/g, "");
 
 	return base + encodeURIComponent(cleanQuery);
 }
@@ -36,5 +33,3 @@ export function getClean(input) {
 	}
 	return cleanQuickStatement(input);
 }
-
-
